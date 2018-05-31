1196c01b7f158086e890ccfc97ac913d769137d1
Alan Liu <yenchiang.liu@gmail.com>
2018-06-01 02:34:32 +0800

    Add naive implementation. Blindly treats all received candidates as valid transactions.

--- assignment2/CompliantNode.java
+++ assignment2/CompliantNode.java
@@ -1,26 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private final double p_graph;
+    private final double p_malicious;
+    private final double p_txDistribution;
+    private final int numRounds;
+    private boolean[] followees;
+    private Set<Transaction> pendingTransactions;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = Arrays.copyOf(followees, followees.length);
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.pendingTransactions = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+        return this.pendingTransactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
+        for (Candidate c: candidates) {
+            this.pendingTransactions.add(c.tx);
+        }
     }
 }
